87e9b41d66b6c54160d5d0d77b476de543a4ef74
˜Guilherme <guilherme.arcanjo300@gmail.com>
2024-10-29 21:01:55 -0300

    feat: first step

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
+import About from "./components/about"
+import { Contact } from "./components/contact"
 import Home from "./components/home"
+import Shoots from "./components/shoots"
 
 function App() {
   return (
     <>
       <Home />
+      <Shoots/>
+      <About/>
+      <Contact/>
     </>
   )
 }
